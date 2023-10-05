3cb333ce956f5d3090ee7500b2906cc2fcc9226a
LAB-USR-LN6377-B0803 <LAB-USR-LN6377-B0803@UTP272943.grupoutp.net>
2023-10-05 14:31:41 -0500

    Actualizar base de datos

--- src/bdaduana/Principal.java
+++ src/bdaduana/Principal.java
@@ -1,11 +1,11 @@
 package bdaduana;
 
+import bdaduana.conexion.BaseDatos;
+
 public class Principal {
+    public static final BaseDatos CONEXION_BD = new BaseDatos();
 
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
-        
-        System.out.println("edoihedjowej");
     }
 }
--- src/bdaduana/conexion/BaseDatos.java
+++ src/bdaduana/conexion/BaseDatos.java
@@ -1,46 +1,48 @@
 package bdaduana.conexion;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.Optional;
 import javax.swing.JOptionPane;
 
 public class BaseDatos {
-    private static final String URL = "jdbc:mysql://localhost:3306/BDAduana";
-    private static final String USUARIO = "root";
-    private static final String CONTRASEÑA = "";
+    private static final String BDAUANA_URL = "jdbc:mysql://localhost:3306/BDAduana";
     
-    private static Connection conexion = null;
+    private Connection conexion = null;
+    private final String url;
     
-    public static Connection getConexion() {
-        if (conexion == null) {
-            try {
-                conexion = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
-            }
-            catch (Exception error) {
-                JOptionPane.showMessageDialog(null, "Error: No se pudo establecer la conexión a la BD! \n" + error);
-            }
-        }
-        return conexion;
+    public BaseDatos(String url) {
+        this.url = url;
     }
     
-    public static Optional<ResultSet> consultaResultSet(String sqlCodigo) {
-        Connection conexion = getConexion();
+    public BaseDatos() {
+        this(BDAUANA_URL);
+    }
+    
+    public void crearConexion() {
+        if (conexion == null) {
+            try {
+                this.conexion = DriverManager.getConnection(this.url, "root", "");
+            }
+            catch (SQLException error) {
+                JOptionPane.showMessageDialog(null, "Error: No se pudo establecer la conexión a la BD! \n" + error);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Error: La conexión ya está establecido!");
+        }
+    }
+    
+    public Optional<ResultSet> ejecutarQuery(String sqlCodigo) {
         try {
             PreparedStatement consulta = conexion.prepareStatement(sqlCodigo);
             return Optional.of(consulta.executeQuery());
         }
         catch (SQLException error) {
-            JOptionPane.showMessageDialog(null, "Error: No se pudo ejectuar el código SQL \n" + error);
+            JOptionPane.showMessageDialog(null, "Error: No se pudo ejectuar el código SQL \n" + error.getMessage());
             return Optional.empty();
         }
     }
     
-    public static int consultaUpdate(String sqlCodigo) {
-        Connection conexion = getConexion();
+    public int ejecutarUpdate(String sqlCodigo) {
         try {
             PreparedStatement consulta = conexion.prepareStatement(sqlCodigo);
             return consulta.executeUpdate();

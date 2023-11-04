7ae9f49ac3a7a4446ce73eb965ef9b447da8c18a
alegregustavo3011 <143915506+alegregustavo3011@users.noreply.github.com>
2023-11-03 22:33:29 -0500

    Update crear_view.sql

--- sql/crear_view.sql
+++ sql/crear_view.sql
@@ -1,6 +1,6 @@
 create view OrdenGeneral
 as
-	select o.OrdenID,c.Consignatario,c.ClienteID as RUC,o.Estado,o.Canal,n.nombreNave as Nave,a.NombreAlmacen as Almacen from Orden o
+	select o.OrdenID,c.Consignatario,c.RUC,o.Estado,o.Canal,n.nombreNave as Nave,a.NombreAlmacen as Almacen from Orden o
     inner join Cliente c on o.ClienteID = c.ClienteID
     inner join Nave n on o.NaveID = n.NaveID
     inner join Almacen a on o.AlmacenID = a.AlmacenID;
